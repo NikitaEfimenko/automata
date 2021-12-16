74dfc84d40115198cb2a2e9a7a9c0d53384e50de
NikitaEfimenko <mszx2000@gmail.com>
2021-12-16 22:18:54 +0300

    fix ipa-example scheme

--- pushdown_example.py
+++ pushdown_example.py
@@ -78,6 +78,13 @@ def automata_factory():
         elif token == '1' and payload == '+':
             instance.memory_stack.pop()
             instance.memory_stack.append('+')
+            instance.memory_stack.append('#')
+        elif token == '1' and payload == '#':
+            instance.memory_stack.pop()
+            instance.memory_stack.append('#')
+        elif token == '0' and payload == '#':
+            instance.memory_stack.pop()
+            instance.memory_stack.append('-')
         else:
             #log_stopped(instance)
             return False
@@ -106,9 +113,14 @@ def automata_factory():
         elif token == '1' and payload == '$':
             instance.memory_stack.pop()
             instance.memory_stack.append('$')
+            instance.memory_stack.append('-')
+            
         elif token == '1' and payload == '+':
             instance.memory_stack.pop()
-            instance.memory_stack.append('+')
+        elif payload == '#':
+            instance.memory_stack.pop()
+            return instance.transition('s6', 'E',
+                                    payload = instance.memory_stack)
         else:
             #log_stopped(instance)
             return False
@@ -145,10 +157,6 @@ def automata_factory():
             if payload == '$':
                 instance.memory_stack.pop()
                 return instance.transition('f5', '$',
-                                        payload = instance.memory_stack)
-            elif payload == '+':
-                instance.memory_stack.pop()
-                return instance.transition('s4', '$',
                                         payload = instance.memory_stack)
             else:
                 return False
@@ -212,25 +220,20 @@ def test(task):
     assert task('01010101111') == True
     assert task('01010101011111') == True
     assert task('01010101010111111') == True
-    assert task('01010100000000000111') == True
+    assert task('010101000000000001111') == True
     assert task('111101') == True    
     assert task('00001') == True
-
     assert task('') == False
     assert task('0000') == False
-
     assert task('0') == False
     assert task('010110') == False
-    ### ---
-    '''
-    assert task('010111') == False
-
     assert task('0101010101111') == False
     assert task('010101010111') == False
-    
-    assert task('1') == False
+    assert task('0101111') == False
+    assert task('010111') == False
     assert task('1111') == False
-    '''
+    assert task('1') == False
+    
     
 if __name__ == '__main__':
     task = IPA.create_task(automata_factory,
